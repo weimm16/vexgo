c7ce4b4b74d6f51700ff6a0d1a750d883d2701e0
atp-gh <atp@disroot.org>
2026-03-10 21:22:44 +0800

    chore: Add flake.nix and direnv to launch a dev environment

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "VexGo development environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit self system;};
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          pnpm
+          nodejs
+          go
+        ];
+      };
+    });
+  };
+}
